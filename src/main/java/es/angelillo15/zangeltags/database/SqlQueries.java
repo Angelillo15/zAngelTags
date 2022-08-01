de3c7beb390510dd5acf1a0035a85820082cf60a
Angel <contact@angelillo15.es>
2022-08-01 13:26:55 +0200

    MySQL bug fix

--- src/main/java/es/angelillo15/zangeltags/database/SqlQueries.java
+++ src/main/java/es/angelillo15/zangeltags/database/SqlQueries.java
@@ -8,8 +8,7 @@ import java.util.UUID;
 
 public class SqlQueries {
     public static boolean userDatatableCreated(Connection connection) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SHOW TABLES LIKE 'userData';");
+        try (PreparedStatement statement = connection.prepareStatement("SHOW TABLES LIKE 'userData';")){
             ResultSet result = statement.executeQuery();
             boolean rb = result.next();
             statement.close();
@@ -22,12 +21,10 @@ public class SqlQueries {
     }
 
     public static boolean tagsTableCreated(Connection connection) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SHOW TABLES LIKE 'tags';");
+        try (PreparedStatement statement = connection.prepareStatement("SHOW TABLES LIKE 'tags';")) {
+            ;
             ResultSet result = statement.executeQuery();
-            boolean rb = result.next();
-            statement.close();
-            return rb;
+            return result.next();
         } catch (SQLException e) {
             Bukkit.getConsoleSender().sendMessage(String.valueOf(e));
         }
@@ -36,10 +33,9 @@ public class SqlQueries {
     }
 
     public static void createUserDataTables(Connection connection) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS `userData` (`ID` INT(11) NOT NULL AUTO_INCREMENT,`UUID` VARCHAR(50) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',`Tag` VARCHAR(100) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',PRIMARY KEY (`ID`) USING BTREE)");
+        try (PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS `userData` (`ID` INT(11) NOT NULL AUTO_INCREMENT,`UUID` VARCHAR(50) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',`Tag` VARCHAR(100) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',PRIMARY KEY (`ID`) USING BTREE)")) {
+
             statement.executeUpdate();
-            statement.close();
 
         } catch (SQLException e) {
             Bukkit.getConsoleSender().sendMessage(String.valueOf(e));
@@ -47,31 +43,29 @@ public class SqlQueries {
     }
 
     public static void createTagsTables(Connection connection) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS `tags` (`name` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',`inGameTag` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',`permission` VARCHAR(200) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci');");
+        try (PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS `tags` (`name` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',`inGameTag` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',`permission` VARCHAR(200) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci');")) {
+
             statement.executeUpdate();
-            statement.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void createUserDataSQLiteTable(Connection connection){
-        try {
-            PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS 'userData' ('ID' INTEGER, 'UUID' TEXT, 'Tag' TEXT, PRIMARY KEY('ID' AUTOINCREMENT));");
+    public static void createUserDataSQLiteTable(Connection connection) {
+        try (PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS 'userData' ('ID' INTEGER, 'UUID' TEXT, 'Tag' TEXT, PRIMARY KEY('ID' AUTOINCREMENT));")) {
+
             statement.executeUpdate();
-            statement.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public static void createTagsDataSQLiteTable(Connection connection){
-        try {
-            PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS 'tags' ('name' TEXT, 'inGameTag' TEXT, 'permission' TEXT);");
+
+    public static void createTagsDataSQLiteTable(Connection connection) {
+        try (PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS 'tags' ('name' TEXT, 'inGameTag' TEXT, 'permission' TEXT);")) {
+
             statement.executeUpdate();
-            statement.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -80,12 +74,10 @@ public class SqlQueries {
 
 
     public static void insertData(Connection connection, UUID uuid, String tag) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO `userData` (`ID`, `UUID`, `Tag`) VALUES (null, ?, ?);");
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO `userData` (`ID`, `UUID`, `Tag`) VALUES (null, ?, ?);")) {
             statement.setString(1, uuid.toString());
             statement.setString(2, tag);
             statement.executeUpdate();
-            statement.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -93,12 +85,10 @@ public class SqlQueries {
     }
 
     public static boolean playerInDB(Connection connection, UUID uuid) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM userData WHERE UUID=?");
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM userData WHERE UUID=?")) {
             statement.setString(1, uuid.toString());
             ResultSet result = statement.executeQuery();
             boolean rb = result.next();
-            statement.close();
 
             return rb;
 
@@ -108,30 +98,26 @@ public class SqlQueries {
     }
 
     public static void updateData(Connection connection, UUID uuid, String tag) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("UPDATE `userData` SET `Tag` = ? WHERE UUID=?");
+        try (PreparedStatement statement = connection.prepareStatement("UPDATE `userData` SET `Tag` = ? WHERE UUID=?")) {
             statement.setString(1, tag);
             statement.setString(2, uuid.toString());
 
             statement.executeUpdate();
-            statement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static String getTag(Connection connection, UUID uuid) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT Tag From `userData` WHERE UUID=?");
+        try (PreparedStatement statement = connection.prepareStatement("SELECT Tag From `userData` WHERE UUID=?")) {
+
 
             statement.setString(1, uuid.toString());
             ResultSet result = statement.executeQuery();
 
             if (result.next()) {
-                String tag = result.getString("Tag");
 
-                statement.close();
-                return tag;
+                return result.getString("Tag");
             }
 
         } catch (SQLException e) {
@@ -159,7 +145,6 @@ public class SqlQueries {
             while (result.next()) {
                 list.add(result.getString(1));
             }
-            statement.close();
             return list;
         } catch (SQLException e) {
             Bukkit.getConsoleSender().sendMessage(String.valueOf(e));
@@ -168,14 +153,13 @@ public class SqlQueries {
     }
 
     public static ArrayList<String> getTagsInGameTag(Connection connection) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags`");
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags`")) {
+
             ResultSet result = statement.executeQuery();
             ArrayList<String> list = new ArrayList<String>();
             while (result.next()) {
                 list.add(result.getString(2));
             }
-            statement.close();
             return list;
         } catch (SQLException e) {
             Bukkit.getConsoleSender().sendMessage(String.valueOf(e));
@@ -184,14 +168,13 @@ public class SqlQueries {
     }
 
     public static ArrayList<String> getTagsPermission(Connection connection) {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags`");
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags`")) {
+
             ResultSet result = statement.executeQuery();
             ArrayList<String> list = new ArrayList<String>();
             while (result.next()) {
                 list.add(result.getString(2));
             }
-            statement.close();
             return list;
         } catch (SQLException e) {
             Bukkit.getConsoleSender().sendMessage(String.valueOf(e));
@@ -199,35 +182,14 @@ public class SqlQueries {
         }
     }
 
-    public static String getTagInGameTag(Connection connection, String tag){
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags` WHERE (name=?)");
-            statement.setString(1, tag);
-            ResultSet result = statement.executeQuery();
-            if(result.next()){
-                String resultString = result.getString("inGameTag");
-                statement.close();
-                return resultString;
-            }else {
-                return "";
-            }
+    public static String getTagInGameTag(Connection connection, String tag) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags` WHERE (name=?)")) {
 
-        } catch (SQLException e) {
-            Bukkit.getConsoleSender().sendMessage(String.valueOf(e));
-            throw new RuntimeException(e);
-        }
-    }
-    public static String getTagPermission(Connection connection, String tag){
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags` WHERE (name=?)");
             statement.setString(1, tag);
             ResultSet result = statement.executeQuery();
-            String rs = result.getString("permission");
-            if(result.next()){
-                statement.close();
-                return rs;
-            }else {
-                statement.close();
+            if (result.next()) {
+                return result.getString("inGameTag");
+            } else {
                 return "";
             }
 
@@ -237,36 +199,47 @@ public class SqlQueries {
         }
     }
 
-    public static void createTag(Connection connection, String name, String inGameTag, String permission){
-        try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO `tags` (`name`, `inGameTag`, `permission`) VALUES (?, ?, ?)");
+    public static String getTagPermission(Connection connection, String tag) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags` WHERE (name=?)")) {
+            statement.setString(1, tag);
+            ResultSet result = statement.executeQuery();
+            if (result.next()) {
+                return result.getString("permission");
+            } else {
+                return "";
+            }
+
+        } catch (SQLException e) {
+            Bukkit.getConsoleSender().sendMessage(String.valueOf(e));
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void createTag(Connection connection, String name, String inGameTag, String permission) {
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO `tags` (`name`, `inGameTag`, `permission`) VALUES (?, ?, ?)")) {
             statement.setString(1, name);
             statement.setString(2, inGameTag);
             statement.setString(3, permission);
             statement.executeUpdate();
-            statement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public static void removeTag(Connection connection, String tag){
-        try {
-            PreparedStatement statement = connection.prepareStatement("DELETE FROM tags WHERE (name=?)");
-            statement.setString(1, tag);
 
+    public static void removeTag(Connection connection, String tag) {
+        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM tags WHERE (name=?)")) {
+            statement.setString(1, tag);
             statement.executeUpdate();
-            statement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public static boolean tagExist(Connection connection, String name){
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags` WHERE (name=?)");
+
+    public static boolean tagExist(Connection connection, String name) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM `tags` WHERE (name=?)")) {
             statement.setString(1, name);
             ResultSet resultSet = statement.executeQuery();
             boolean rb = resultSet.next();
-            statement.close();
             return rb;
         } catch (SQLException e) {
             throw new RuntimeException(e);
