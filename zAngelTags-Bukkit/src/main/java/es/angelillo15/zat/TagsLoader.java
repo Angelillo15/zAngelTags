844f996ba2e2b736ec93cdd440cc02219807106c
Angelillo15 <contact@angelillo15.es>
2023-05-13 23:09:18 +0200

    Fill loader

--- zAngelTags-Bukkit/src/main/java/es/angelillo15/zat/TagsLoader.java
+++ zAngelTags-Bukkit/src/main/java/es/angelillo15/zat/TagsLoader.java
@@ -3,11 +3,18 @@ package es.angelillo15.zat;
 public class TagsLoader extends TagsPlugin {
     @Override
     public void onEnable() {
-
+        loadLogger();
+        drawLogo();
+        loadConfig();
+        registerCommands();
+        registerListeners();
+        loadDatabase();
     }
 
     @Override
     public void onDisable() {
-
+        unregisterCommands();
+        unregisterListeners();
+        unloadDatabase();
     }
 }
