fd2ef9471ee2a6b618cb69dfd8eaea07e63ee632
Angelillo15 <contact@angelillo15.es>
2023-05-13 22:43:34 +0200

    Added dependencies to the api

--- zAngelTags-API/build.gradle.kts
+++ zAngelTags-API/build.gradle.kts
@@ -12,7 +12,17 @@ repositories {
 }
 
 dependencies {
-
+    compileOnly("org.spigotmc:spigot-api:1.13-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.2")
+    compileOnly("com.github.mrgraycat:eGlow:master-SNAPSHOT")
+    compileOnly("net.byteflux:libby-bukkit:1.1.5")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:core:4.1.10")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:configurablegui:4.1.10")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:advancedslot:4.1.10")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:pagination:4.1.10")
+    compileOnly("ru.vyarus:yaml-config-updater:1.4.2")
+    compileOnly("org.yaml:snakeyaml:1.33")
+    compileOnly("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.3")
 }
 
 blossom {
