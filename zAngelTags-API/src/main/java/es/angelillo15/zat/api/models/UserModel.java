efabececea16dd226120063fbf2b72a40daa1daa
Angelillo15 <contact@angelillo15.es>
2023-05-14 12:34:54 +0200

    Added UserData getters

--- zAngelTags-API/src/main/java/es/angelillo15/zat/api/models/UserModel.java
+++ zAngelTags-API/src/main/java/es/angelillo15/zat/api/models/UserModel.java
@@ -8,7 +8,9 @@ import com.craftmend.storm.api.StormModel;
 import es.angelillo15.zat.api.TagsInstance;
 import es.angelillo15.zat.api.database.PluginConnection;
 
-@Table(name="zat_user_data")
+import java.util.UUID;
+
+@Table(name = "zat_user_data")
 public class UserModel extends StormModel {
     public static final String UNKNOWN = "unknown";
     @Column(
@@ -23,13 +25,19 @@ public class UserModel extends StormModel {
     )
     private String username = UNKNOWN;
 
+    /**
+     * Get the UUID of the user
+     * @param where UUID or username
+     * @param value UUID or username value
+     * @return the UUID of the user
+     */
     public static boolean exists(String where, String value) {
         Storm storage = PluginConnection.getStorm();
         boolean exists = false;
 
         try {
             exists = storage.buildQuery(UserModel.class)
-                    .where(where, Where.EQUAL ,value)
+                    .where(where, Where.EQUAL, value)
                     .execute()
                     .join()
                     .iterator()
@@ -39,5 +47,65 @@ public class UserModel extends StormModel {
         }
 
         return exists;
+    }
+
+    /**
+     * Check if user exists in database
+     * @param uuid UUID of the user
+     * @return true if exists, false if not
+     */
+    public static boolean exits(UUID uuid) {
+        return exists("UUID", uuid.toString());
+    }
+
+    /**
+     * Check if user exists in database
+     * @param username username of the user
+     * @return true if exists, false if not
+     */
+    public static boolean exits(String username) {
+        return exists("username", username);
+    }
+
+    /**
+     * Get user from database
+     * @param where UUID or username
+     * @param value UUID or username value
+     * @return the {@link UserModel} instance
+     */
+    public static UserModel getUser(String where, String value) {
+        Storm storage = PluginConnection.getStorm();
+        UserModel user = null;
+
+        try {
+            user = storage.buildQuery(UserModel.class)
+                    .where(where, Where.EQUAL, value)
+                    .execute()
+                    .join()
+                    .iterator()
+                    .next();
+        } catch (Exception e) {
+            TagsInstance.getLogger().debug("Error while getting user: " + e.getMessage());
+        }
+
+        return user;
+    }
+
+    /**
+     * Get user from database
+     * @param uuid UUID of the user
+     * @return the {@link UserModel} instance
+     */
+    public static UserModel getUser(UUID uuid) {
+        return getUser("UUID", uuid.toString());
+    }
+
+    /**
+     * Get user from database
+     * @param username username of the user
+     * @return the {@link UserModel} instance
+     */
+    public static UserModel getUser(String username) {
+        return getUser("username", username);
     }
 }
