dc8d26fc6bdd8119da80573f66314d8e448ad564
Angelillo15 <contact@angelillo15.es>
2023-05-14 13:20:53 +0200

    Unused import - net.byteflux.libby.Library.

--- zAngelTags-Bukkit/src/main/java/es/angelillo15/zat/utils/LibsLoader.java
+++ zAngelTags-Bukkit/src/main/java/es/angelillo15/zat/utils/LibsLoader.java
@@ -3,7 +3,6 @@ package es.angelillo15.zat.utils;
 import es.angelillo15.zat.TagsPlugin;
 import es.angelillo15.zat.api.libs.LibsManager;
 import net.byteflux.libby.BukkitLibraryManager;
-import net.byteflux.libby.Library;
 
 public class LibsLoader {
     public static void load() {
