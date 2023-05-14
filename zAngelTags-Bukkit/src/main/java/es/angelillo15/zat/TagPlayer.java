735688c67da07fd56c6f6ba6b7f640b87d64817b
Angelillo15 <contact@angelillo15.es>
2023-05-14 13:40:58 +0200

    Fill new methods from parent interface

--- zAngelTags-Bukkit/src/main/java/es/angelillo15/zat/TagPlayer.java
+++ zAngelTags-Bukkit/src/main/java/es/angelillo15/zat/TagPlayer.java
@@ -2,6 +2,8 @@ package es.angelillo15.zat;
 
 import es.angelillo15.zat.api.ITagPlayer;
 import es.angelillo15.zat.api.models.TagModel;
+import es.angelillo15.zat.api.models.UserModel;
+import es.angelillo15.zat.api.models.UserTagsModel;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nullable;
@@ -9,8 +11,20 @@ import javax.annotation.Nullable;
 public class TagPlayer implements ITagPlayer {
     private final Player player;
     private TagModel tag;
+    private UserModel userModel;
+    private UserTagsModel userTagsModel;
     public TagPlayer(Player player) {
         this.player = player;
+        reloadData();
+    }
+
+    @Override
+    public void reloadData() {
+        this.userModel = UserModel.getUser(player.getUniqueId());
+        this.userTagsModel = userModel.getTags();
+        if (userTagsModel.getTagModelId() != null) {
+            this.tag = userTagsModel.getTagModel();
+        }
     }
 
     @Override
@@ -42,5 +56,15 @@ public class TagPlayer implements ITagPlayer {
     @Override
     public boolean hasTag(TagModel tag) {
         return getTag() != null && getTag().equals(tag);
+    }
+
+    @Override
+    public UserModel getUserModel() {
+        return userModel;
+    }
+
+    @Override
+    public UserTagsModel getUserTagsModel() {
+        return userTagsModel;
     }
 }
